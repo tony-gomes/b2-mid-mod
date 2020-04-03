80bf4bfe0f347ac084558b5e8796451f616a767d
Anthony Gomes <code@tonygom.es>
2020-04-03 10:44:16 -0600

    Add rides index spec file

---
+++ spec/features/rides/index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  before(:each) do
+    @ride_1 = Ride.create(name: "Skull Crusher",
+                          thrill_rating: 3)
+
+    @ride_2 = Ride.create(name: "Paradise Falls",
+                          thrill_rating: 9)
+
+    @ride_3 = Ride.create(name: "Head Basket",
+                          thrill_rating: 7)
+  end
+
+  context 'When I visit a rides index page' do
+    context 'I see a header saying All Rides' do
+      it "And I see a list of all rides names & thrill rating" do
+
+        visit "/rides"
+
+        within ".main-rides-block" do
+          expect(page).to have_content("Skull Crusher")
+          expect(page).to have_content("3")
+
+          expect(page).to have_content("Paradise Falls")
+          expect(page).to have_content("9")
+
+          expect(page).to have_content("Head Basket")
+          expect(page).to have_content("7")
+        end
+      end
+    end
+  end
+end
